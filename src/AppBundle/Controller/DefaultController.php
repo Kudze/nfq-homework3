1fa97ff2c1fcc864c0bf173d2675e73195aeb024
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-10-18 23:41:00 +0300

    Added genrateAction

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
@@ -15,6 +17,29 @@ class DefaultController extends Controller
     {
 
         return $this->render('default/index.html.twig', []);
+
+    }
+
+    /**
+     * @Route("/generate", name="generate")
+     */
+    public function generateAction() {
+
+        $category = new Category();
+        $category->setTitle('Geriausia kategorija');
+
+        $product = new Product();
+        $product->setTitle('Geriausias produktas');
+        $product->setActive(true);
+        $product->setCategory($category);
+        $product->setPrice('69 Eurai');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return $this->render('default/generate.html.twig', []);
+
 
     }
 }
